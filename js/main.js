bab8c09901a1b67b4d6ebc4a04415b05595e0998
Miroslav Ranisavljevic <caffeweb@gmail.com>
2016-08-01 16:27:01 +0200

    Dosta izmena final

--- js/main.js
+++ js/main.js
@@ -32,6 +32,34 @@ $(document).ready(function () {
         $('.twelve-p').hide();
         $('.twelve').show();
     });
+    //PRODUCT DETAILS SLIDER
+    $("aside.preview nav").show();
+	var previewImg = $("img#main");
+	$("a:first").addClass("active");
+	$("nav a img").click(function(){
+		var link = $(this).parent();	
+		var linkHref = link.attr("href");			
+		var linkAlt = link.attr("alt");			
+		if( $(link).hasClass("active") == false)
+		{
+			$("a").removeClass("active");
+			link.addClass("active");											
+			$(previewImg).animate({
+				opacity: 0.8,
+			}, 300, function() {
+				previewImg.attr("src", linkHref);
+				previewImg.attr("alt", linkAlt);				
+				$(this).animate({
+					opacity: 1,
+					}, 300
+				);							
+			});			
+		}
+		return false;
+	});
+	$("input").click(function(){
+		$("p.more").fadeIn("slow");
+	});
 
 
 });
