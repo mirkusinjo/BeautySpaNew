87a0b79e4f815a491da77f6254ff61bcb81e1567
Miroslav Ranisavljevic <caffeweb@gmail.com>
2016-08-08 23:38:12 +0200

    Dodat modal na home page, stilizovan tabela.

--- js/main.js
+++ js/main.js
@@ -24,49 +24,53 @@ $(document).ready(function () {
                 " - $" + $("#slider-range").slider("values", 1));
     });
     //PICTURE VIEW
-    $('.displayView .fa-th-list').click(function(){
+    $('.displayView .fa-th-list').click(function () {
         $('.twelve-p').show();
         $('.twelve').hide();
     });
-     $('.displayView .fa-th').click(function(){
+    $('.displayView .fa-th').click(function () {
         $('.twelve-p').hide();
         $('.twelve').show();
     });
     //PRODUCT DETAILS SLIDER
     $("aside.preview nav").show();
-	var previewImg = $("img#main");
-	$("a:first").addClass("active");
-	$("nav a img").click(function(){
-		var link = $(this).parent();	
-		var linkHref = link.attr("href");			
-		var linkAlt = link.attr("alt");			
-		if( $(link).hasClass("active") == false)
-		{
-			$("a").removeClass("active");
-			link.addClass("active");											
-			$(previewImg).animate({
-				opacity: 0.8,
-			}, 300, function() {
-				previewImg.attr("src", linkHref);
-				previewImg.attr("alt", linkAlt);				
-				$(this).animate({
-					opacity: 1,
-					}, 300
-				);							
-			});			
-		}
-		return false;
-	});
-	$("input").click(function(){
-		$("p.more").fadeIn("slow");
-	});
-        //SHOW HIDE DISPLAY VIEW
-        $('span.fa-cog').click(function(){
-           $('.filtersSelect').toggle();
-        });
+    var previewImg = $("img#main");
+    $("a:first").addClass("active");
+    $("nav a img").click(function () {
+        var link = $(this).parent();
+        var linkHref = link.attr("href");
+        var linkAlt = link.attr("alt");
+        if ($(link).hasClass("active") == false)
+        {
+            $("a").removeClass("active");
+            link.addClass("active");
+            $(previewImg).animate({
+                opacity: 0.8,
+            }, 300, function () {
+                previewImg.attr("src", linkHref);
+                previewImg.attr("alt", linkAlt);
+                $(this).animate({
+                    opacity: 1,
+                }, 300
+                        );
+            });
+        }
+        return false;
+    });
+    $("input").click(function () {
+        $("p.more").fadeIn("slow");
+    });
+    //SHOW HIDE DISPLAY VIEW
+    $('span.fa-cog').click(function () {
+        $('.filtersSelect').toggle();
+    });
 //SHOPING CART ONCLICK
-$(".shopingCart").click(function(){
+    $(".shopingCart").click(function () {
         $(".table").toggle();
+    });
+    $('.remove').click(function () {
+        $(this).parent().parent().hide();
+
     });
 
 });
